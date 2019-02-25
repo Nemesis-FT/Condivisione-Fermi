58b1040427e3b901474c8690a93e320f0d638a89
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-02-25 17:37:43 +0100

    Test login page

--- tests.py
+++ tests.py
@@ -6,9 +6,20 @@ import server
 def app():
     app = server.app
     app.debug = True
+    app.config["TESTING"] = True
     return app.test_client()
 
 
 def test_login_page(app):
     res = app.get("/login")
     assert res.status_code == 200
+    res = app.post("/login", data={
+        "password": "haha"
+    })
+    assert res.status_code == 400
+    res = app.post("/login", data={
+        "username": "sacripante"
+    })
+    assert res.status_code == 400
+    res = app.post("/login")
+    assert res.status_code == 400
